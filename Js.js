2deadda7cb7510f45e9421ccef2473e2c778a24a
MRskullissue <atboinikeeplay@gmail.com>
2025-12-13 14:08:17 +0300

    create 3 files

--- Js.js
+++ Js.js
@@ -1,3 +1,10 @@
-
+let block = document.querySelector('.comments')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+     let dy = e.pageY - window.innerHeight / 2
+     let angleX = 20 * dx / window.innerWidth / 2
+     let angleY = 20 * dY / window.innerHeight / 2
+     block.style.transform = `rotateX(${angleX}deg)  rotateY(${angleY}deg) ` 
+})
 
 
